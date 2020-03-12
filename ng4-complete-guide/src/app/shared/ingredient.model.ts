b7b48decef107c6f0d2789aa48e25ea5d1b9e9d8
Zuhair Ahamed <zuhair.ahamed@cambio.lk>
2020-03-12 16:59:14 +0530

    first commit

---
+++ ng4-complete-guide/src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient{
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name:string, amount: number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }  
+    // We can use above methodology and below methodology
+
+    constructor(public name: string,public amount: number){}
+}
